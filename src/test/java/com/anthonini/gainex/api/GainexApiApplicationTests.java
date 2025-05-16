a3dfd5b9f447420abc789358376c7df9efde6634
Anthonini <anthonini@info.ufrn.br>
2025-05-16 05:26:33 -0300

    Initial commit

---
+++ src/test/java/com/anthonini/gainex/api/GainexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anthonini.gainex.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GainexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
