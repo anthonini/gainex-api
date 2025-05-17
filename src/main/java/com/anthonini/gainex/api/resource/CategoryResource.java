3966ab513d880252dfb73225efa46c5e257c6802
Anthonini <anthonini@info.ufrn.br>
2025-05-17 06:17:32 -0300

    Category entity and resource created

---
+++ src/main/java/com/anthonini/gainex/api/resource/CategoryResource.java
@@ -0,0 +1,32 @@
+package com.anthonini.gainex.api.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anthonini.gainex.api.model.Category;
+import com.anthonini.gainex.api.repository.CategoryRepository;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResource {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	@GetMapping
+	public List<Category> list() {
+		return categoryRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Category> findByCode(@PathVariable Long id) {
+		Category category = categoryRepository.findById(id).orElse(null);
+		return category != null ? ResponseEntity.ok(category) : ResponseEntity.notFound().build();
+	}
+}
