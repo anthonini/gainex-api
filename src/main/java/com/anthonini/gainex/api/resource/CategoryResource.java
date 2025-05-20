897ddf863faa1e43a9f3d87a54c16a25a5c8a5a1
Anthonini <anthonini@info.ufrn.br>
2025-05-20 06:50:50 -0300

    Implemented category resource POST mehtod

--- src/main/java/com/anthonini/gainex/api/resource/CategoryResource.java
+++ src/main/java/com/anthonini/gainex/api/resource/CategoryResource.java
@@ -3,14 +3,19 @@ package com.anthonini.gainex.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anthonini.gainex.api.model.Category;
 import com.anthonini.gainex.api.repository.CategoryRepository;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/categories")
@@ -28,5 +33,11 @@ public class CategoryResource {
 	public ResponseEntity<Category> findByCode(@PathVariable Long id) {
 		Category category = categoryRepository.findById(id).orElse(null);
 		return category != null ? ResponseEntity.ok(category) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping	
+	public ResponseEntity<Category> create(@RequestBody Category category, HttpServletResponse response) {
+		Category createdCategory = categoryRepository.save(category);	
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdCategory);
 	}
 }
