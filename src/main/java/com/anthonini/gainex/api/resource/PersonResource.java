450e492664addb5009e7bb4583d25b19a3d2b664
Anthonini <anthonini@info.ufrn.br>
2025-05-21 06:24:49 -0300

    Implemented Person resource delete

--- src/main/java/com/anthonini/gainex/api/resource/PersonResource.java
+++ src/main/java/com/anthonini/gainex/api/resource/PersonResource.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anthonini.gainex.api.event.CreatedResourceEvent;
@@ -39,5 +41,11 @@ public class PersonResource {
 		Person createdPerson = personRepository.save(Person);
 		publisher.publishEvent(new CreatedResourceEvent(this, response, createdPerson.getId()));
 		return ResponseEntity.status(HttpStatus.CREATED).body(createdPerson);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long id) {
+		personRepository.deleteById(id);
 	}
 }
