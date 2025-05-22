965beeb109250136d76504fead7ac7ec36b57fee
Anthonini <anthonini@gmail.com>
2025-05-22 05:41:10 -0300

    Implemented DataIntegrityViolationException handling.
    Signed-off-by: Anthonini <anthonini@gmail.com>

--- src/main/java/com/anthonini/gainex/api/exceptionhandler/GainexExceptionHandler.java
+++ src/main/java/com/anthonini/gainex/api/exceptionhandler/GainexExceptionHandler.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -51,6 +53,14 @@ public class GainexExceptionHandler extends ResponseEntityExceptionHandler {
 		String developerMessage = ex.toString();
 		List<Error> errors = Arrays.asList(new Error(userMessage, developerMessage));
 		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
+	
+	@ExceptionHandler({ DataIntegrityViolationException.class } )
+	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
+		String userMessage = messageSource.getMessage("resource.operation-not-permitted", null, LocaleContextHolder.getLocale());
+		String developerMessage = ExceptionUtils.getRootCauseMessage(ex);
+		List<Error> errors = Arrays.asList(new Error(userMessage, developerMessage));
+		return handleExceptionInternal(ex, errors, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
 	private List<Error> createErrorsList(BindingResult bindingResult) {
